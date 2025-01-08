2c4f0bcfa14343f397589ab2f6d99aeb24e1bdd7
jimm9 Tran <jimm9tran@gmail.com>
2025-01-08 14:56:21 +0700

    Find+Sort+Pagination

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,11 +1,52 @@
 import { Router, Request, Response } from "express";
+
 import Task from "../models/task.model";
+import paginationHelper from "../../../helpers/pagination";
 
 
 export const index = async (req: Request, res: Response) => {
-    const tasks = await Task.find({
+    // Find 
+    interface Find {
+        deleted: boolean,
+        status?: string,
+    }
+
+    const find: Find = {
         deleted: false,
-    });
+    }
+
+    if(req.query.status) {
+        find.status = req.query.status as string;
+    }
+    // End Find 
+
+    // Pagination
+    let initPagination = {
+        currentPage: 1,
+        limitItems: 2,
+    }
+
+    const countTasks = await Task.countDocuments(find);
+    const objectPagination = paginationHelper(
+        initPagination,
+        req.query,
+        countTasks,
+    );
+    // End Pagination
+
+    // Sort
+    const sort = {};
+
+    if(req.query.sortKey && req.query.sortValue) {
+        const sortKey = req.query.sortKey as string;
+        sort[sortKey] = req.query.sortValue as string;
+    }
+    // End Sort
+
+    const tasks = await Task.find(find)
+    .sort(sort)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
     
     res.json(tasks);
 }
