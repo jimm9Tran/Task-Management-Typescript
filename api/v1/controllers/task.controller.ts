a72e1ef28bc5125cd0c88becaa7525664c500b81
jimm9 Tran <jimm9tran@gmail.com>
2025-01-08 10:47:54 +0700

    MVC - Controller

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,17 +1,16 @@
 import { Router, Request, Response } from "express";
-const router: Router = Router();
-
 import Task from "../models/task.model";
 
-router.get("/", async (req: Request, res: Response) => {
+
+export const index = async (req: Request, res: Response) => {
     const tasks = await Task.find({
         deleted: false,
     });
     
     res.json(tasks);
-});
+}
 
-router.get("/detail/:id", async (req: Request, res: Response) => {
+export const detail = async (req: Request, res: Response) => {
     const id: string = req.params.id;
 
     const task = await Task.findOne({
@@ -20,6 +19,5 @@ router.get("/detail/:id", async (req: Request, res: Response) => {
     });
     
     res.json(task);
-});
+}
 
-export const taskRoutes: Router = router;
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import * as databasee from "./config/database";
-import mainV1Routes from "./v1/routes/index.route";
+import mainV1Routes from "./api/v1/routes/index.route";
 
 dotenv.config();
 databasee.connect();
@@ -10,6 +10,7 @@ const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
 mainV1Routes(app);
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
